195bc461e260fa29e50e54d7c6d88c2bebc81344
alphagit88 <alphagit88@gmail.com>
2026-05-31 17:21:02 +0530

    par lvl, mlt whs, fxd cnflcts with sup admin

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://svsoft.io/';
+const BASE_URL = 'https://supervendor.io/';
 //const BASE_URL = 'http://192.168.8.120:5000/';
 
 const normalizedBaseUrl = BASE_URL.replace(/\/+$/, '');
